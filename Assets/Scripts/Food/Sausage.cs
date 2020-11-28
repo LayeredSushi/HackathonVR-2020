2436b594f81b2f2517e60b1234594c00f323798b
Roman <sfilmak@gmail.com>
2020-11-28 19:11:37 +0200

    Кастрюли и картошечка для сыночка

---
+++ Assets/Scripts/Food/Sausage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sausages : FoodItem
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello, I am sausage");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
