c96367cc877aefb644838e93253b26ca9bd5eeee
Golbez <palart2000@gmail.com>
2020-11-28 19:41:34 +0100

    added temp

--- Assets/Scripts/Food/FoodItem.cs
+++ Assets/Scripts/Food/FoodItem.cs
@@ -5,5 +5,11 @@ public abstract class FoodItem : Grabbable
     
     public float temperature;
     public float maximumTemperature;
+    public bool isSpoiled = false;
 
+    private void Update()
+    {
+        if (temperature > maximumTemperature)
+            isSpoiled = true;
+    }
 }
