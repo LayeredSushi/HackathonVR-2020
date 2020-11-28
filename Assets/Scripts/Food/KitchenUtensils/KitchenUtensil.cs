3de71a8c60f6fa8ef31786bfcacd5afbf671fbd7
Yutasushied1 <75175086+Yutasushied1@users.noreply.github.com>
2020-11-28 18:48:39 +0100

    Scene2

---
+++ Assets/Scripts/Food/KitchenUtensils/KitchenUtensil.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class KitchenUtensil : Grabbable
+{
+    protected float temperature;
+    protected FoodItem foodItem;
+}
