7449d9e648db6486acafb157527b071cf003a8a4
Yutasushied1 <75175086+Yutasushied1@users.noreply.github.com>
2020-11-28 23:53:46 +0100

    a

---
+++ Assets/Scripts/Food/KitchenUtensils/FridgeSpawnUnit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeSpawnUnit : MonoBehaviour
+{
+    public FoodItem[] prefabs;
+    public int index;
+
+    private FridgeSpawnUnit[] fridgeSpawnUnits;
+    void Start()
+    {
+        fridgeSpawnUnits = GetComponentsInChildren<FridgeSpawnUnit>();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Instantiate(prefabs[index]);
+    }
+}
